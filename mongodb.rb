0d02a4ef956a94406ba03d9fb96c3e81944a8e2c
nayakpalak93 <nayakpalak93@gmail.com>
2015-10-31 03:39:41 +0000

    firstmysqlpush

---
+++ mongodb.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/ruby
+
+# Copyright (c) 2015 ObjectLabs Corporation
+# Distributed under the MIT license - http://opensource.org/licenses/MIT
+
+# Written with mongo 1.9.2
+# Documentation: http://api.mongodb.org/ruby/
+# A ruby script connecting to a MongoDB database given a MongoDB Connection URI.
+
+require "mongo"
+require "json"
+
+# -------------------------------------------------my code starts here
+
+### Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+uri = "mongodb://<mydata>:<7070>@ds045664.mongolab.com:45664/heroku_5x71tdz7"
+
+client = Mongo::MongoClient.from_uri(uri)
+
+db = client.db(heroku_5x71tdz7)
+
+#fetching data from data sourse api and storing it intoo data variable
+data = JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=editors&api_key=4d7847876fa96f67f881aaf1b73e0e30&format=json").read)
+
+#add data intodatabase
+
+i = 1
+data["similarartists"]["artist"].each { |x|
+  db.collection("atristsList").insert_one({ID: i.to_s, Title: x["name"]})
+  var += 1;
+}
+
+
+#fatchind data from database 
+# 1)by primary key:
+
+puts db.users.find( { 'id' => { '$eq'=> 9 } }, { 'ID'=> '1', 'Title'=> '1' } ).limit(100)
+
+songs.find({ 'weeksAtOne' => { '$gte' => 10 } }).sort('decade', 1)
+# 2)by non primary key
+
+puts db.users.find( { 'Title'=> { '$eq'=> 'The National' } }, { 'ID'=> '1', 'Title'=> '1' } )
+
+#---------------------------------------------------my code ends here
+
+
+
---
+++ mysql.rb
@@ -0,0 +1,31 @@
+require "dbi"
+
+#retrieve data from data source.
+data = JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=editors&api_key=4d7847876fa96f67f881aaf1b73e0e30&format=json").read)
+
+begin
+     # connect to the MySQL server
+     dbh = DBI.connect("postgres://wsfxsporhicczg:0P2B-Jg4GQ5BjP1CuB270ZI0y4@ec2-107-21-219-235.compute-1.amazonaws.com:5432/dakvjenroh6bs8", 
+	                    "wsfxsporhicczg", "0P2B-Jg4GQ5BjP1CuB270ZI0y4")
+	 
+	 i=1
+	 data["similarartists"]["artist"].each { |x|
+	 q=""
+	 value=q+x["name"]+q
+	     dbh.do("INSERT INTO TABLE ARTIST VALUES(#{i},#{value})")
+	     i+=1
+	 }
+	 puts "record added succesfully tomysql database........"
+	 dbh.commit
+	 
+rescue DBI::DatabaseError => e
+     puts "An error occurred"
+     puts "Error code:    #{e.err}"
+     puts "Error message: #{e.errstr}"
+     dbh.rollback
+ensure
+     # disconnect from server
+     dbh.disconnect if dbh
+end
+	 
+	 
