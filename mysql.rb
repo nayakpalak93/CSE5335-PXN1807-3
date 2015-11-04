567f9022cab8e292443282a2684e9605c9bc25eb
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-04 02:20:40 +0000

    kbjk

--- mysql.rb
+++ mysql.rb
@@ -1,8 +1,8 @@
 require 'pg'
   
 # Output a table of current connections to the DB
-conn = pg.connect( dbname: 'dakvjenroh6bs8',user:'wsfxsporhicczg',password:'0P2B-Jg4GQ5BjP1CuB270ZI0y4' )
-
+#conn = PG.connect( dbname: 'dakvjenroh6bs8',user:'wsfxsporhicczg',password:'0P2B-Jg4GQ5BjP1CuB270ZI0y4' )
+con = PG.connect :dbname => 'dakvjenroh6bs8', :user => 'wsfxsporhicczg', :password => '0P2B-Jg4GQ5BjP1CuB270ZI0y4', :host =>'ec2-107-21-219-235.compute-1.amazonaws.com'
 #fetching data from data sourse api and storing it intoo data variable
 data = JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=editors&api_key=4d7847876fa96f67f881aaf1b73e0e30&format=json").read)
 
