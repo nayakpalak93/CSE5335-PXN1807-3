45b1095a699cccebf4c861deb216f82addca60e9
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-06 23:38:38 +0000

    jghj

--- myredis.rb
+++ myredis.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'open-uri'
 require 'redis'
 
-redis = Redis.new(:host => "ec2-54-83-199-200.compute-1.amazonaws.com", :port => 14259, :url => "redis://h:p2d92tnpl3a3i695fj0h7us73nl@ec2-54-83-199-200.compute-1.amazonaws.com:14259", :password => "mysecret")
+redis = Redis.new(:host => "ec2-54-83-199-200.compute-1.amazonaws.com", :port => 14259, :url => "redis://h:p2d92tnpl3a3i695fj0h7us73nl@ec2-54-83-199-200.compute-1.amazonaws.com:14259", :password => "p2d92tnpl3a3i695fj0h7us73nl")
 
 #fatch fromdata sourse API and store in a variable
 result = JSON.parse(open("https://cdph.data.ca.gov/api/views/yijp-bauh/rows.json?accessType=DOWNLOAD").read)
