33ab148a7971310cee16154b6fa8ba96865fdf81
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-06 00:49:21 +0000

    jghj

--- mysql.rb
+++ mysql.rb
@@ -2,13 +2,13 @@ require 'pg'
 require 'json'
 require 'open-uri'
   
-db=Mongo::Client.new(['ds045664.mongolab.com:45664'], :database => 'heroku_5x71tdz7', :user => 'testuser', :password => '7070')
+con = PG.connect :dbname => 'dakvjenroh6bs8', :user => 'wsfxsporhicczg', :password => '0P2B-Jg4GQ5BjP1CuB270ZI0y4', :host =>'ec2-107-21-219-235.compute-1.amazonaws.com'
 
 result = JSON.parse(open("https://cdph.data.ca.gov/api/views/yijp-bauh/rows.json?accessType=DOWNLOAD").read)
 
 puts "Creating Table.........................................................."
 create_table="CREATE TABLE SQLTABLE(Id INTEGER PRIMARY KEY, year INTEGER,sex TEXT)"
-db.exec(query)
+con.exec(query)
 
 puts "Inserting values into SQLTABLE.........................................."
 $i=1
@@ -17,7 +17,7 @@ while $i < 110 do
     year=result["data"][$i][10]
     sex=result["data"][$i][11]
     insert_table="INSERT INTO TABLE VALUES('#{i}','#{year}','#{sex}')"
-    db.exec(insert_table)
+    con.exec(insert_table)
     i+=1
 
 end
