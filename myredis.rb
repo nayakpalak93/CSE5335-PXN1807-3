b9c85a1309eefa9df1edb511e53c467dc52ec0fe
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-06 23:49:43 +0000

    jghj

--- myredis.rb
+++ myredis.rb
@@ -7,5 +7,5 @@ redis = Redis.new(:host => "ec2-54-83-199-200.compute-1.amazonaws.com", :port =>
 #fatch fromdata sourse API and store in a variable
 result = JSON.parse(open("https://cdph.data.ca.gov/api/views/yijp-bauh/rows.json?accessType=DOWNLOAD").read)
 
-redis['foo'] = 'bar'
-puts redis['foo']
+redis.hset 'key','value1'
+redis.hgetall 'key'
