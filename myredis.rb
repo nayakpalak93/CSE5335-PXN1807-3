bd3ee0c54ce42b46d1d2963a61955cf4c7fa1cf7
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-06 23:35:39 +0000

    jghj

--- myredis.rb
+++ myredis.rb
@@ -2,16 +2,10 @@ require 'json'
 require 'open-uri'
 require 'redis'
 
+redis = Redis.new(:host => "ec2-54-83-199-200.compute-1.amazonaws.com", :port => 14259, :url => "redis://h:p2d92tnpl3a3i695fj0h7us73nl@ec2-54-83-199-200.compute-1.amazonaws.com:14259", :password => "mysecret")
+
 #fatch fromdata sourse API and store in a variable
 result = JSON.parse(open("https://cdph.data.ca.gov/api/views/yijp-bauh/rows.json?accessType=DOWNLOAD").read)
 
-$i=1
-while $i < 110 do
-
-HSET mykey index '#{$i}'
-HSET mykey year result["data"][$i][10]
-HSET mykey sex result["data"][$i][11] 
-
-end
-
-HGETALL mykey
+redis.set "foo", [1, 2, 3].to_json
+JSON.parse(redis.get("foo"))
