23eef8c32600121f7ae64bb50d46985aa42d4a7e
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-06 04:56:57 +0000

    jghj

--- mysql.rb
+++ mysql.rb
@@ -8,7 +8,7 @@ con = PG.connect :dbname => 'dakvjenroh6bs8', :user => 'wsfxsporhicczg', :passwo
 result = JSON.parse(open("https://cdph.data.ca.gov/api/views/yijp-bauh/rows.json?accessType=DOWNLOAD").read)
 
 puts "Creating Table.........................................................."
-create_table="CREATE TABLE IF NOT EXISTS MYSQLTABLE(Id INTEGER PRIMARY KEY, year INTEGER,sex TEXT)"
+create_table="CREATE TABLE IF NOT EXISTS MYSQLTABLE(index INTEGER PRIMARY KEY, year INTEGER,sex TEXT)"
 con.exec(create_table)
 con.exec("DELETE FROM MYSQLTABLE")
 puts "Inserting values into SQLTABLE.........................................."
@@ -24,18 +24,18 @@ while $i < 110 do
 end
 puts "Values inserted into SQLTABLE successfully..............................."
 
-puts "fatching value from table where ID=7 ....................................."
+puts "fatching value from table where primary key index=7 ....................................."
 
-fatchById="SELECT year,sex,Id FROM MYSQLTABLE WHERE Id=7"
+fatchById="SELECT year,sex,Id FROM MYSQLTABLE WHERE index=7"
 
 rs=con.exec(fatchById)
 
 my_results = {}
 
 rs.each do |row|
-     my_results["Id"] = row['Id']
-  my_results["year"] = row['year']
-  my_results["sex"] = row['sex']
+     my_results["Index"] = row['index']
+  my_results["Year"] = row['year']
+  my_results["Sex"] = row['sex']
 end
 
  
@@ -50,9 +50,9 @@ rs1=con.exec(fatchByYear)
 result={}
 
 rs1.each do |row|
-    result["ID"]=row['Id']
-  result["year"] = row['year']
-  result["sex"] = row['sex']
+    result["Index"]=row['index']
+  result["Year"] = row['year']
+  result["Sex"] = row['sex']
 end
 
 puts result
