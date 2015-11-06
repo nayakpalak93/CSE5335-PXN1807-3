22f73064962d149b7352821b2d2b64e8a4739ec7
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-06 00:34:52 +0000

    jghj

--- mysql.rb
+++ mysql.rb
@@ -1,22 +1,24 @@
 require 'pg'
 require 'JSON'
   
-# Output a table of current connections to the DB
-#conn = PG.connect( dbname: 'dakvjenroh6bs8',user:'wsfxsporhicczg',password:'0P2B-Jg4GQ5BjP1CuB270ZI0y4' )
-con = PG.connect :dbname => 'dakvjenroh6bs8', :user => 'wsfxsporhicczg', :password => '0P2B-Jg4GQ5BjP1CuB270ZI0y4', :host =>'ec2-107-21-219-235.compute-1.amazonaws.com'
-#fetching data from data sourse api and storing it intoo data variable
-data = JSON.parse(open("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=editors&api_key=4d7847876fa96f67f881aaf1b73e0e30&format=json").read)
+  
+db=Mongo::Client.new(['ds045664.mongolab.com:45664'], :database => 'heroku_5x71tdz7', :user => 'testuser', :password => '7070')
 
-int i=0
-create_table="CREATE TABLE ARTISTS(Id INTEGER PRIMARY KEY, Name TEXT)"
-conn.exec(query)
-data["similarartists"]["artist"].each{|artist|
-    value=artist["name"]
-    insert_table="INSERT INTO TABLE VALUES('#{i}','#{value}')"
-    conn.exec(insert_table)
+result = JSON.parse(open("https://cdph.data.ca.gov/api/views/yijp-bauh/rows.json?accessType=DOWNLOAD").read)
+
+puts "Creating Table.........................................................."
+create_table="CREATE TABLE SQLTABLE(Id INTEGER PRIMARY KEY, year INTEGER,sex TEXT)"
+db.exec(query)
+
+puts "Inserting values into SQLTABLE.........................................."
+$i=1
+while $i < 110 do
+
+    year=result["data"][$i][10]
+    sex=result["data"][$i][11]
+    insert_table="INSERT INTO TABLE VALUES('#{i}','#{year}','#{sex}')"
+    db.exec(insert_table)
     i+=1
-}
 
-
-
-
+end
+puts "Values inserted into SQLTABLE successfully..............................."
