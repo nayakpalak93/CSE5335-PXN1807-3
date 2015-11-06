5cce34854096adcda35eaf7b26b8df7c5caf7432
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-06 05:06:45 +0000

    jghj

--- mysql.rb
+++ mysql.rb
@@ -8,7 +8,7 @@ con = PG.connect :dbname => 'dakvjenroh6bs8', :user => 'wsfxsporhicczg', :passwo
 result = JSON.parse(open("https://cdph.data.ca.gov/api/views/yijp-bauh/rows.json?accessType=DOWNLOAD").read)
 
 puts "Creating Table.........................................................."
-create_table="CREATE TABLE IF NOT EXISTS MYSQLTABLE(index INTEGER PRIMARY KEY, year INTEGER,sex TEXT)"
+create_table="CREATE TABLE IF NOT EXISTS MYSQLTABLE(index INTEGER, year INTEGER,sex TEXT)"
 con.exec(create_table)
 con.exec("DELETE FROM MYSQLTABLE")
 puts "Inserting values into SQLTABLE.........................................."
@@ -26,7 +26,7 @@ puts "Values inserted into SQLTABLE successfully..............................."
 
 puts "fatching value from table where primary key index=7 ....................................."
 
-fatchById="SELECT * FROM MYSQLTABLE WHERE Id=7"
+fatchById="SELECT * FROM MYSQLTABLE WHERE index=7"
 
 rs=con.exec(fatchById)
 
