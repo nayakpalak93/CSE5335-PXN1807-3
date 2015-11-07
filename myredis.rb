97e9ecbcc56d76ecd1a1b6d3ab217f6038660579
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-07 00:16:15 +0000

    jghj

--- myredis.rb
+++ myredis.rb
@@ -7,5 +7,12 @@ redis = Redis.new(:host => "ec2-54-83-199-200.compute-1.amazonaws.com", :port =>
 #fatch fromdata sourse API and store in a variable
 result = JSON.parse(open("https://cdph.data.ca.gov/api/views/yijp-bauh/rows.json?accessType=DOWNLOAD").read)
 
-redis.hset 'mykey','key','value1'
-puts redis.hgetall 'mykey'
+$i=1
+while $i < 110 do
+    $year=result["data"][$i][10]
+    $sex=result["data"][$i][11]
+redis.hset "row#{$i}","index",$i
+redis.hset "row#{$i}","year", $year
+redis.hset "row#{$i}","year", $sex
+puts redis.hgetall "row#{$i}"
+end
