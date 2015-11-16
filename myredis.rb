c4e9d2f4cf2af0398c1528f76f04e0593de660cd
nayakpalak93 <nayakpalak93@gmail.com>
2015-11-16 00:25:01 +0000

    wdw

--- myredis.rb
+++ myredis.rb
@@ -30,17 +30,14 @@ puts redis.hgetall "row#{$index}"
 
 
 puts "Enter a year to get data of that year"
-#$y=gets.chomp
+$y=gets.chomp
 $n=1
-=begin
+
 while $n < 110 do
-$temp_y=redis.hget "row#{$n}", "year"
+$temp_y=redis.hget("row#{$n}", "year")
 if $temp_y == $y
     puts redis.hget "row#{$n}"
 end
 $n+=1
 end
 
-=end
-
-puts redis.hget("row#{$n}", "year")
