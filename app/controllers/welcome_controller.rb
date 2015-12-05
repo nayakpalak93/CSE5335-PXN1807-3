e1bcaa1a6fbd7ec6ced9c39ca3a8ee9c9f8e3cad
nayakpalak93 <nayakpalak93@gmail.com>
2015-12-05 06:06:39 +0000

    asdasd

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
   end
 
 def getData
-  
+  db=Mongo::Client.new(['ds045664.mongolab.com:45664'], :database => 'heroku_5x71tdz7', :user => 'testuser', :password => '7070')
   id=params[:id]
 
   db[:heroku_5x71tdz7].find('unique' => id).each {|data1| 
